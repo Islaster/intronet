316a769aaa03f4d34d88ed97c28210603314adb1
Isaac Laster <isaac.laster812@gmail.com>
2025-12-27 21:39:34 -0800

    routing and filetree system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,9 @@ import { lazy, Suspense } from "react";
 import "./layout.css";
 import Footer from "./components/shared/footer";
 const JobBoard = lazy(() => import("./pages/jobBoard/page"));
-const Dashboard = lazy(() => import("./pages/dashboard"));
+const Dashboard = lazy(() => import("./pages/dashboard/page"));
+const LinkedinConnect = lazy(() => import("./pages/linkedinConnect/page"));
+const JobTracker = lazy(() => import("./pages/JobTracker/page"));
 import Navbar from "./components/shared/navbar";
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/board" element={<JobBoard />} />
+            <Route path="/connect" element={<LinkedinConnect />} />
+            <Route path="tracker" element={<JobTracker />} />
           </Routes>
         </Suspense>
       </main>
---
+++ client/src/pages/JobTracker/page.tsx
@@ -0,0 +1,8 @@
+export default function JobTracker() {
+  /*
+        1. table of all the jobs you applied to
+        2. different headers (job name, applied, reached out for refferals, etc.)
+        3. checkbox for each step in the process.
+    */
+  return <></>;
+}
